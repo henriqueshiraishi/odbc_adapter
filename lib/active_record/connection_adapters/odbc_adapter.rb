babc371d3c3523c224bfd31eacdecd173732475f
Henrique Shiraishi <henriqueashiraishi@gmail.com>
2025-09-04 15:56:13 +0000

    Add: store connection and raw_connection in ODBCAdapter

--- lib/active_record/connection_adapters/odbc_adapter.rb
+++ lib/active_record/connection_adapters/odbc_adapter.rb
@@ -92,6 +92,8 @@ module ActiveRecord
         configure_time_options(connection)
         super(connection, logger, config)
         @database_metadata = database_metadata
+        @connection = connection
+        @raw_connection = connection
       end
 
       # Returns the human-readable name of the adapter.
